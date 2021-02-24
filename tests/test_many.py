997f7e9b584f4bf9dfd7355b7ebcee99b3f5cf91
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-02-24 13:58:59 -0800

    add other complicated graph

--- tests/test_many.py
+++ tests/test_many.py
@@ -26,3 +26,27 @@ def test_infinity():
 
     node = most_detrimental(graph, 0, 5)
     assert node == 1
+
+
+# In this graph, any of the nodes could be the most damaging! All should have damage 0
+# because there is an alternate path that doesn't include them
+
+# GRAPH:
+# 0->1 u
+# 0->2 u
+# 2->3 d
+# 1->3 d
+# 1->4 d
+# 3->5 u
+# 4->5 d
+
+# SOURCE: 0
+# SINK:   5
+
+def test_infinity():
+    # create a more complicated graph
+    graph = Graph([(0,1), (0,2), (2,3), (1,3), (1,4), (3,5), (4,5)], directed=True)
+    graph.es['type'] = ['up', 'up', 'down', 'down', 'down', 'up', 'down']
+
+    node = most_detrimental(graph, 0, 5)
+    assert node in (1, 2, 3, 4)
